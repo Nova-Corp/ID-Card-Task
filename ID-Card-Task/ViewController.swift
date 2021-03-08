ed034822f5d7ca97c3b6ca1751e34f71f0006c4b
Shanmugarajeshwaran R <shanmugarajeshwaranr@gmail.com>
2021-03-08 17:40:55 +0530

    Initial Commit

---
+++ ID-Card-Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ID-Card-Task
+//
+//  Created by ADMIN on 08/03/21.
+//  Copyright © 2021 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
